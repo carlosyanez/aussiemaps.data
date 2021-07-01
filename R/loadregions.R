3c1622b15fb5a693f42649397fefbcbb189d7093
carlosyanez <carlos.yanez.s@gmail.com>
2021-07-01 09:36:12 +0200

    Create loadregions.R

---
+++ R/loadregions.R
@@ -0,0 +1,6 @@
+#' Load list of regions
+#' @return list of regions
+#' @export loadregions
+loadregions <- function(){
+        load(system.file("extdata", "regions.rda", package = "aussiemaps.data"))
+}
